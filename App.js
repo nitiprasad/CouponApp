c950a24b4fa5b057ac5c627df4820a3339d0a951
Nanzoo <nihaal.bhojwani@gmail.com>
2021-07-08 22:32:28 -0400

    Yo

--- App.js
+++ App.js
@@ -5,16 +5,28 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>First page!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const IMAGE_URLS =
+{
+  sale:
+  'https://images.unsplash.com/photo-1607083206968-13611e3d76db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1089&q=80',
+};
+
+const setBackground = (image)
+{
+  document.body.style.background = "url('"+IMAGE_URLS.[image]+"')";
+};
+setBackground('sale');
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
